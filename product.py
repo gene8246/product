2d36f1eef7cbf11347cb5d970042493ae864b40f
Jeffrey <gene8246@gmail.com>
2022-08-11 00:35:44 +0900

    original

---
+++ product.py
@@ -0,0 +1,14 @@
+products = []
+while True:
+	product = input('please enter product name: ')
+	if product == 'q':
+		break
+	price = input('pelase enter product price: ')
+	#p = []
+	#p.append(product)
+	#p.append(price)
+		#p = [product, price] 相同於上面三行
+	products.append([product, price]) #相同於上面
+
+print(products)
+print(products[0][0]) #第零個小清單中的第零個
